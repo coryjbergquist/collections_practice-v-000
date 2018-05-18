68bbac6e72475eeef95e4ea6b46e25535ecbba97
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 22:31:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 def sort_array_asc(array)
   array.sort
@@ -20,5 +21,5 @@ array.reverse!
 end
 
 def kesha_maker(array)
-array.map!.with_index { |letter|  % 3 == 0 ? letter : "$"}
+array.each { |letter|  % 3 == 0 ? letter : "$"}
 end
