105afac5f41b5e7f74f531f12b3ce1c60e3c0349
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 21:40:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,7 @@ end
 def swap_elements(array)
   array [0], array[1], array [2] = array [0], array [2], array [1]
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index] = destination_index
+end
