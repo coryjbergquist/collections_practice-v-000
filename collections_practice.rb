d64fc8adf53ff52c2bcb6756ee3a8ea1fb2789fd
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-18 21:26:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,14 @@ end
 def sort_array_char_count(array)
 array.sort {|x,y| x.length <=> y.length}
 end
+
+def swap_elements(array)
+array.sort {|x,y| 
+  if x.index == 3
+    1
+elsif y.index = 4
+  -1
+else
+  0
+  end
+end
